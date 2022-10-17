48e56a6ceca631e44009606b10f102d8cf1fc6ea
TaufikBima <114376358+TaufikBima@users.noreply.github.com>
2022-10-17 21:22:30 +0700

    Add files via upload

---
+++ RataNilai.java
@@ -0,0 +1,16 @@
+package sesi4;
+
+public class RataNilai{
+   int x1, x2, x3;
+   double y1, y2;
+
+   public int average(int x1, int x2) {
+      return (x1+x2)/2;
+   }
+   public double average(double y1, double y2) {
+      return (y1+y2)/2;
+   }
+   public int average(int x1, int x2, int x3) {
+      return (x1+x2+x3)/3;
+   }
+}
