9110987ab6ec06ff3645c58bb1f84a32d227ad77
TaufikBima <114376358+TaufikBima@users.noreply.github.com>
2022-10-17 21:23:17 +0700

    Add files via upload

---
+++ Tugas.java
@@ -0,0 +1,10 @@
+package sesi4;
+
+public class Tugas {
+   public static void main(String args[]){
+      RataNilai rn = new RataNilai();
+      System.out.println("Rata-rata nilai 21 dan 13 adalah : " + rn.average(21, 13));
+      System.out.println("Rata-rata nilai 19.3 dan 39.5 adalah : " + rn.average(19.3, 39.5));
+      System.out.println("Rata-rata nilai 123, 567 dan 744 adalah : " + rn.average(123, 567, 744));
+   }
+}
