9d99328d98d12db3735e7ab42428351a48dc9aa0
TaufikBima <114376358+TaufikBima@users.noreply.github.com>
2022-10-17 21:25:29 +0700

    Add files via upload

---
+++ Hewan.java
@@ -0,0 +1,12 @@
+package Sesi4;
+class Hewan {
+   public void move() {
+      System.out.println("Hewan Bisa Bergerak");
+   }
+}
+
+class Anjing extends Hewan {
+   public void move() {
+      System.out.println("Anjing Bisa Berjalan dan Berlari");
+   }
+}
